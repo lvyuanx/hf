b4296de057984171b23e8f451e8e1e272eda7a3a
lyx <2466057319@qq.com>
2023-05-23 23:02:51 +0800

    :bug: 修复build脚本打包问题

--- build/build.py
+++ build/build.py
@@ -13,7 +13,7 @@ class BuildObj(object):
     def get_args(self):
         parser = argparse.ArgumentParser(description='Build vue to django project')
         parser.add_argument('-i', '--input_path', help='输入路径', required=False,
-                            default='D:\\Project\\git\\hf\\client\\admin\\static')
+                            default='D:\\Project\\git\\hf\\client\\admin')
         parser.add_argument('-o', '--out_path', help='输出路径', required=False,
                             default='D:\\Project\\git\\hf\\service\\project')
         parser.add_argument('-c', '--clear', help='清理目标路径', required=False, default=False)
@@ -24,18 +24,18 @@ class BuildObj(object):
 
     def clear_path(self):
         if self.clear:
-            web_dir = os.path.join(self.out_path, 'web','static')
+            web_dir = os.path.join(self.out_path, 'web', 'static')
             if os.path.exists(web_dir):
                 shutil.rmtree(web_dir)
 
     def npm_run_build(self):
-        os.system('npm run build')
+        os.system("cd %s && npm run build" % self.input_path)
 
     def migration_dist(self):
-        dict_dir = self.input_path
-        web_staitc_dir = os.path.join(self.out_path, 'web', 'static')
+        dict_dir = os.path.join(self.input_path, 'static')
+        web_static_dir = os.path.join(self.out_path, 'web', 'static')
 
-        shutil.copytree(dict_dir, web_staitc_dir)
+        shutil.copytree(dict_dir, web_static_dir)
 
     def start(self):
         self.get_args()
